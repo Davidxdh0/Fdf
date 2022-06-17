72a8db03d44943f2fea91e84d0f42972ce05d25b
Davidxdh0 <Davidxdh0@users.noreply.github.com>
2022-06-17 20:39:21 +0200

    add libft+gnl

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strjoin.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dyeboa <dyeboa@student.codam.nl>             +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/04/16 14:26:29 by dyeboa        #+#    #+#                 */
+/*   Updated: 2022/06/15 10:39:38 by dyeboa        ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strjoin(const char *s1, const char *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!s2 && !s1)
+		return (NULL);
+	str = malloc((ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	while (s1 && s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	free((char *)s1);
+	return (str);
+}
